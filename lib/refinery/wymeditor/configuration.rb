fb8e840172556fce4ce2b6c24d09565662840040
Philip Arndt <parndt@gmail.com>
2012-02-24 14:08:10 +1300

    Moved more support files from Refinery CMS

--- lib/refinery/wymeditor/configuration.rb
+++ lib/refinery/wymeditor/configuration.rb
@@ -2,9 +2,9 @@ module Refinery
   module Wymeditor
     include ActiveSupport::Configurable
 
-    config_accessor :wymeditor_whitelist_tags
+    config_accessor :whitelist_tags
 
-    self.wymeditor_whitelist_tags = {}
+    self.whitelist_tags = {}
 
   end
 end
--- lib/refinery/wymeditor/engine.rb
+++ lib/refinery/wymeditor/engine.rb
@@ -6,6 +6,15 @@ module Refinery
       isolate_namespace Refinery
       engine_name :refinery_wymeditor
 
+      # set the manifests and assets to be precompiled
+      initializer "refinery.wymeditor.assets.precompile" do |app|
+        app.config.assets.precompile += [
+          "wymeditor/lang/*",
+          "wymeditor/skins/refinery/*",
+          "wymeditor/skins/refinery/**/*"
+        ]
+      end
+
       config.after_initialize do
         Refinery.register_engine(Refinery::Wymeditor)
       end

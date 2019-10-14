371ee969504c0b9ef061846d4d79d4d6c6ed79bc
Brice Sanchez <bricesanchez@users.noreply.github.com>
2019-10-14 00:12:58 -0400

    Now use assets manifest

---
+++ app/assets/config/refinery_wymeditor_manifest.js
@@ -0,0 +1,9 @@
+//= link wymeditor.css
+//= link theme.css
+
+//= link refinery/wymeditor.js
+
+//= link wymeditor/skins/refinery/skin.js
+//= link wymeditor/skins/refinery/skin.css
+
+//= link wymeditor/skins/refinery/wymiframe.css

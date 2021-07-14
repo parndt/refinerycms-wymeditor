2d1c33afb066a21ba729487a23256c9167d1a127
Maarten Bezemer <maarten.bezemer@gmail.com>
2021-07-15 01:27:26 +0200

    Add support for precompiled assets (#53)
    Fixes #52

---
+++ app/helpers/asset_helper.rb
@@ -0,0 +1,9 @@
+module AssetHelper
+  def asset_exists?(path)
+    if Rails.configuration.assets.compile
+      Rails.application.precompiled_assets.include? path
+    else
+      Rails.application.assets_manifest.assets[path].present?
+    end
+  end
+end
